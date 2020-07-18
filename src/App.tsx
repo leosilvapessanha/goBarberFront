b8ab4674c2ec2991f256694e5b10007f3c0a9fb5
Leonardo Pessanha <leonardosilvapessanha@gmail.com>
2020-07-18 20:02:02 -0300

    index created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global'
+import SignIn from './pages/SignIn/index'
 
 const App: React.FC = () => (
   <>
-    <h1>Hey mate!</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 )
