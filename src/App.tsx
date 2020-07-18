0e8d3bb7b4957faea2290e104e4b32f5b5963c43
Leonardo Pessanha <leonardosilvapessanha@gmail.com>
2020-07-18 18:11:33 -0300

    CSS done, issue with app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Hey, Mate</h1>
+import GlobalStyle from './styles/global'
+
+const App: React.FC = () => (
+  <>
+    <h1>Hey mate!</h1>
+    <GlobalStyle />
+  </>
+)
 
 export default App;
